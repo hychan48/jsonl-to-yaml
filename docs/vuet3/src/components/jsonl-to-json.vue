c21757b381ac8fc42ee2c5f9cb8071e38edc9066
JC <jchan48@gmail.com>
2024-01-29 00:24:00 -0500

    added fixture files... probably needed for further testing... including the intermediate steps

--- docs/vuet3/src/components/jsonl-to-json.vue
+++ docs/vuet3/src/components/jsonl-to-json.vue
@@ -17,13 +17,22 @@
         </v-col>
         <!--                <v-col cols="auto">-->
         <v-col cols="12">
+          <!--          todo make this computed with get/set because json to yaml should be easy as well... -->
+
           <v-textarea
             label="JSON"
             _rows="10"
             auto-grow
             variant="outlined"
-
             v-model="strJSON"
+          />
+        </v-col>
+        <v-col cols="12">
+          <v-textarea
+            v-model="strYaml"
+            auto-grow
+            label="YAML"
+            variant="outlined"
           />
         </v-col>
 
@@ -34,25 +43,50 @@
 </template>
 
 <script setup>
-import { jsonl2json } from "##/src/index.mjs"
+import {json2Yaml, jsonl2json} from "##/src/index.mjs"
 /** @type {string} singleLineJSONL */
 import { singleLineJSONL } from "##/tests/fixtures/smoke/single-line.mjs"
 // import { multilineJSONL } from "##/tests/fixtures/smoke/single-line.mjs"
-import { ref, computed } from "vue";
+import {ref, computed} from "vue";
+// import {ref, computed, reactive} from "vue";
 // setup jsonl input and strJSON output as strings
 const jsonl = ref(singleLineJSONL);
 // const jsonl = ref(multilineJSONL);
 // const strJSON = ref("");
-const strJSON = computed(() => {
-  try{
+// const oJSON = reactive({});//todo make this a computed property
+const oJSON = computed(() => {
+  try {
     //make the replacer and/or space configurable in the future... also maybe just make a component and put it in a lib.
-    return JSON.stringify(jsonl2json(jsonl.value), null, 2);
+    return jsonl2json(jsonl.value);
 
-  }catch (e) {
-    return e.message;
+  } catch (e) {
+    return e;
   }
   // return JSON.stringify(jsonl.value);
 });
+const strJSON = computed(() => {
+  return JSON.stringify(oJSON.value, null, 2);
+  // return JSON.stringify(jsonl.value);
+});
+const strYaml = computed(() => {
+  /* if oJSON is error return blank */
+  if (oJSON.value instanceof Error) {
+    return "Error";
+  } else {
+    return json2Yaml(oJSON.value);
+  }
+
+});
+// const strYaml = computed(() => {
+//   try{
+//     //make the replacer and/or space configurable in the future... also maybe just make a component and put it in a lib.
+//     return JSON.stringify(jsonl2json(jsonl.value), null, 2);
+//
+//   }catch (e) {
+//     return e.message;
+//   }
+//   // return JSON.stringify(jsonl.value);
+// });
 /* computed property to convert jsonl to json */
 
 
