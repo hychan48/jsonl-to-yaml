b0ce0293c3986b4732c18bd78ae1077191266909
JC <jchan48@gmail.com>
2024-01-28 20:21:53 -0500

    fixed resolve. so it actually can import now

--- docs/vuet3/vite.config.mjs
+++ docs/vuet3/vite.config.mjs
@@ -37,7 +37,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '##': fileURLToPath(new URL('./../..', import.meta.url)),
+      '##': fileURLToPath(new URL('./../..', import.meta.url)),//project root
     },
     extensions: [
       '.js',
