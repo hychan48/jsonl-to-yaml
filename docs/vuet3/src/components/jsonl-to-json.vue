012c9f9ceceebbf17d2654b59804641b8fcabbf3
JC <jchan48@gmail.com>
2024-01-29 00:13:47 -0500

    fixed jsonl to textarea

--- docs/vuet3/src/components/jsonl-to-json.vue
+++ docs/vuet3/src/components/jsonl-to-json.vue
@@ -1,15 +1,17 @@
+<!-- actually jsonl to json to yaml... rename later -->
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
 
       <div class="text-body-2 font-weight-light mb-n1">@hychan48</div>
-      <h1 class="text-h2 font-weight-bold">jsonl-to-json</h1>
+      <h1 class="text-h2 font-weight-bold">jsonl-to-json-to-yaml</h1>
       <v-row class="d-flex align-center justify-center">
         <v-col cols="12">
-          <v-text-field
+          <v-textarea
             label="JSONL"
-            outlined
-            rows="10"
+            auto-grow
+            variant="outlined"
+
             v-model="jsonl"
           />
         </v-col>
@@ -17,8 +19,10 @@
         <v-col cols="12">
           <v-textarea
             label="JSON"
-            outlined
-            rows="10"
+            _rows="10"
+            auto-grow
+            variant="outlined"
+
             v-model="strJSON"
           />
         </v-col>
@@ -31,10 +35,13 @@
 
 <script setup>
 import { jsonl2json } from "##/src/index.mjs"
+/** @type {string} singleLineJSONL */
 import { singleLineJSONL } from "##/tests/fixtures/smoke/single-line.mjs"
+// import { multilineJSONL } from "##/tests/fixtures/smoke/single-line.mjs"
 import { ref, computed } from "vue";
 // setup jsonl input and strJSON output as strings
 const jsonl = ref(singleLineJSONL);
+// const jsonl = ref(multilineJSONL);
 // const strJSON = ref("");
 const strJSON = computed(() => {
   try{
